cd8808afac665c1b9f8ba28135dd4f4d129a7dea
Alejandro <aletomasperren@gmail.com>
2024-09-10 12:50:28 -0300

    commit

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
+const passport = require('passport');
 const { database } = require('./keys');
 
 // Inicializaciones
 const app = express();
-
+require('./lib/passport')
 // Settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
@@ -36,6 +37,8 @@ app.use(session({
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Variables Globales
 
--- src/routes/autenticaciones.js
+++ src/routes/autenticaciones.js
@@ -1,6 +1,19 @@
 const express = require('express');
-
 const router = express.Router();
+const passport = require('../lib/passport');
+router.get('/signup',(req,res) =>{
+    res.render('auth/signup')
+});
 
+router.post('/signup',(req,res) =>{
+    passport.authenticate('local.signup',{
+        successRedirect: '/profile'
+    } )
+    
+});
+
+router.get('/profile', (req,res) =>{
+    res.send('este es tu perfil')
+})
 
 module.exports = router;
