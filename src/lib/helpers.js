9501b2b60822c6cf10aae391b188318314fdb137
Alejandro <aletomasperren@gmail.com>
2024-10-06 03:02:10 -0300

    so

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
 const helpers = {};
 
-helpers.encryptPassword = async (password) =>{
+helpers.encryptPassword = async (password) => {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     return hash;
--- src/lib/passport.js
+++ src/lib/passport.js
@@ -10,21 +10,21 @@ passport.use('local.signin', new LocalStrategy({
     passReqToCallback: true
 }, async (req, username, password, done) => {
 
-   const rows = await  pool.query('SELECT * FROM users WHERE username = ?', [username]);
-    if (rows.length > 0){
+    const rows = await pool.query('SELECT * FROM users WHERE username = ?', [username]);
+    if (rows.length > 0) {
         const user = rows[0];
-       const validPassword = await helpers.matchPassword(password, user.password);
-       if(validPassword){
-        done(null, user, req.flash('success' + 'Bienvenido' + user.username));
-       }else {
-        done(null, false, req.flash('message'+'Contraseña Incorrecta'))
-       }
-    }else{
-        return done(null, false, req.flash('message'+'El nombre de Usuario no Existe'))
+        const validPassword = await helpers.matchPassword(password, user.password);
+        if (validPassword) {
+            done(null, user, req.flash('success' + 'Bienvenido' + user.username));
+        } else {
+            done(null, false, req.flash('message' + 'Contraseña Incorrecta'))
+        }
+    } else {
+        return done(null, false, req.flash('message' + 'El nombre de Usuario no Existe'))
     }
 }));
 
-    
+
 passport.use('local.signup', new LocalStrategy({
     usernameField: 'username',
     passwordField: 'password',
